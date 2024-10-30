e8cd97851df0a273d05ed3a1548dc52b935ea739
Areefa-Cader <areefacader12345@gmail.com>
2024-10-31 00:57:51 +0530

    almost UI done

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -3,7 +3,16 @@ import React from "react";
 
 const LoginSignup = ()=>{
     return(
-        <div>
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign up</h1>
+                <div className="signup-field">
+                    <input type="text" name="" id="" placeholder="Enter you name"/>
+                    <input type="email" name="" id="" placeholder="Your Email" />
+                    <input type="password" name="" id="" placeholder="Passowrd"/>
+                </div>
+                <button>Continue</button>
+            </div>
 
 
         </div>
